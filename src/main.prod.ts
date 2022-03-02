9aef346881d1a77e206c0e3c22dfc0cbe8080fb3
euberdeveloper <euberdeveloper@gmail.com>
2022-03-02 02:05:59 +0100

    chore: remove unused main.prod
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/main.prod.ts
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import '@/plugins/vuetify';
-import '@/plugins/api';
-
-import wrap from '@vue/web-component-wrapper';
-import PncGroupRegistration from '@/webcomponents/PncGroupRegistration.vue';
-
-const CustomElement = wrap(Vue, PncGroupRegistration);
-window.customElements.define('pnc-group-registration', CustomElement as unknown as CustomElementConstructor);
